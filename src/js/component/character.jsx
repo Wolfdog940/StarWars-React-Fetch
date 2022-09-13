bd1b783b2063fe0d78b671233ba0d455e1b0440d
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-09-13 17:17:37 +0000

    infocard

--- src/js/component/character.jsx
+++ src/js/component/character.jsx
@@ -2,38 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
-
 const Character = (props) => {
-    const {item,index,type} =props;
-    
+  const { item, index, type,} = props;
 
- 
-   
-  
-  
-   
- 
-   
-    
   return (
     <div className="card  bg-dark text-warning">
-       
-       <img src={"https://starwars-visualguide.com/assets/img/characters/"+(props.index+1)+".jpg"
-              //https://starwars-visualguide.com/assets/img/characters/1.jpg
-            }  />
+      <img
+        src={
+          "https://starwars-visualguide.com/assets/img/characters/" +
+          (props.index + 1) +
+          ".jpg"
+        }
+      />
       <div className="card-body">
         <h5 className="card-title">{item.name}</h5>
         <div className="card-text">
           <div>Gender : {item.gender} </div>
           <div>Hair Color : {item.hair_color}</div>
           <div>Eye Color: {item.eye_color} </div>
+          <div>index: {index} </div>
         </div>
-        <Link to={"/info/"+index+"/"+type}>
-					<button className="btn btn-primary">More info!</button>
-				</Link>
+        <Link to={"/info/" + index + "/" + type}>
+          <button className="btn btn-primary">More info!</button>
+        </Link>
       </div>
     </div>
-    
   );
 };
 
--- src/js/component/info.jsx
+++ src/js/component/info.jsx
@@ -1,54 +1,69 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 
 import { Home } from "../views/home";
 
 import "../../styles/home.css";
 import { useParams } from "react-router-dom";
-
-
+import { useState } from "react";
 
 const Info = (props) => {
-    
-    const{store,actions}=useContext(Context);
 
-    let {posicion,type}= useParams();
-
-    let planetas=store.planets[posicion];
-    let personajes=store.character[posicion];
-    let vehiculos=store.vehicles[posicion];
-   
-    
-   
- console.log(type)
-   
-   
-    
-    
-   
  
-   
-    
-  return (
-    (type=="characters")?
-    (<div className="info">
-      <div className="text-warning">{personajes?.name}</div>
-    <div className="text-warning">{personajes?.gender}</div>
-    </div>):((type=="planets")?(<div className="info">
-      <div className="text-warning">{planetas?.name}</div>
-    <div className="text-warning">{planetas?.terrain}</div>
-    </div>):(<div className="info">
-      <div className="text-warning">{vehiculos?.name}</div>
-    <div className="text-warning">{vehiculos?.model}</div>
-    </div>))
-    
-    
 
-    
+  
+
+ 
+
+  const { store, actions } = useContext(Context);
+  
+
+  let { posicion,type} = useParams();
+
+  let planetas = store.planets[posicion];
+  let personajes = store.character[posicion];
+  let vehiculos = store.vehicles[posicion];
+
+  console.log("index")
+
+  return type == "characters" ? (
+    <div className="info">
+      <div className="text-warning">{personajes?.name}</div>
+      <div className="text-warning">{personajes?.gender}</div>
+      
+      <img
+        src={
+          "https://starwars-visualguide.com/assets/img/characters/" +
+          (parseInt(posicion)+1)+
+          ".jpg"
+        }
+      />
+    </div>
+  ) :type == "planets" ? (
+    <div className="info">
+      <div className="text-warning">{planetas?.name}</div>
+      <div className="text-warning">{planetas?.terrain}</div>
+      <img
+        src={
+          "https://starwars-visualguide.com/assets/img/planets/" +
+          (posicion) +
+          ".jpg"
+        }
+      />
+    </div>
+  ) :type == "vehicles"(
+    <div className="info">
+      <div className="text-warning">{vehiculos?.name}</div>
+      <div className="text-warning">{vehiculos?.model}</div>
+      <img
+         src={
+          "https://starwars-visualguide.com/assets/img/planets/" +
+          (posicion) +
+          ".jpg"
+        }
+      />
+    </div>
   );
 };
-
-
-
 
 export default Info;
